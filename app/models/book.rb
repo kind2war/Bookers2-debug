d71f933d9eb8da40ac08b8e98d2fb14a7dd47b24
kind2war <130664778+kind2war@users.noreply.github.com>
2023-06-19 03:36:54 +0000

    [add]いいね機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :title, presence:true
   validates :body, presence:true,length:{maximum:200}
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+      #引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうかを調べる。
+      #存在していればtrue、存在していなければfalseを返す
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
+      #単数形にすると、/:idがURLに含まれなくなる。
+      #コメント機能とは異なり、いいね機能の場合は「1人のユーザーは1つの投稿に対して1回しかいいねできない」という仕様であるため、
+      #ユーザーidと投稿idが分かれば、どのいいねを削除すればいいのかが特定できdestroyできる。
+      #そのため、いいねのidはURLに含める必要がない(params[:id]を使わなくても良い)ため、
+      #resourcesではなくresourceを使ってURLに/:idを含めない形にする。
   end
   resources :users, only: [:index,:show,:edit,:update]
 

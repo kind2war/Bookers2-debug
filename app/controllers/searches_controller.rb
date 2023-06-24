49de7d8b9a014a66da769deb74dc66cb22513c05
kind2war <130664778+kind2war@users.noreply.github.com>
2023-06-24 23:14:23 +0000

    [fix]検索機能,後方一致の修,ほかレイアウト

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,13 +3,13 @@ class SearchesController < ApplicationController
 
   def search
     @range = params[:range]
-
     if @range == "User"
-      @users = User.looks(params[:search], params[:word])
-      render "searches/search_result"
-    else
-      @books = Book.looks(params[:search], params[:word])
-      render "searches/search_result"
+       @users = User.looks(params[:search], params[:word])
+       @keyword = params[:word]
+       render "searches/search_result"
+    else @books = Book.looks(params[:search], params[:word])
+          @keyword = params[:word]
+          render "searches/search_result"
     end
   end
 end

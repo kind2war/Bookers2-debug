540ffdb43ac3a6171b359c76c3bd6d441e630228
kind2war <130664778+kind2war@users.noreply.github.com>
2023-06-25 03:01:07 +0000

    favとコメの非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,20 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
+    #redirect_to request.referer
       #まずbookを特定する。
       #ログイン中のﾕｰｻﾞｰとしてbook.idに紐づける形でfavoritesを新たに１つだけ作る。
       #保存する
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
 end
